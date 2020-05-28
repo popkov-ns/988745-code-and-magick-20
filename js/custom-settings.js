a599b885562c09f08779d24ba82bdc849b21ebaf
Nikita Popkov <popkov-ns@mail.ru>
2020-05-28 14:08:48 +0300

    Правки 3.0

--- js/custom-settings.js
+++ js/custom-settings.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 window.fireballSize = 22;
 window.wizardSpeed = 3;
@@ -6,16 +6,16 @@ window.wizardWidth = 70;
 
 window.getFireballSpeed = function (movingLeft) {
   return movingLeft ? 5 : 2;
-}
+};
 
 window.getWizardHeight = function (width) {
   return 1.337 * width;
-}
+};
 
 window.getWizardX = function (width) {
-  return (width - wizardWidth) / 2;
-}
+  return (width - window.wizardWidth) / 2;
+};
 
 window.getWizardY = function (height) {
   return height / 3;
-}
+};
