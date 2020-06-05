31e7d32c7a1b5b3786e023ee6e371563a97b231f
Nikita Popkov <popkov-ns@mail.ru>
2020-06-05 13:43:31 +0300

    Добавление времени прохождении игры

--- js/stat.js
+++ js/stat.js
@@ -17,12 +17,18 @@ var BAR_MAX_HEIGHT = 150;
 var BAR_WIDTH = 40;
 var BAR_DISTANCE = 50 + BAR_WIDTH;
 
-var renderCloud = function (ctx, x, y, color) {
+function renderCloud(ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-};
+}
 
-var getMaxElement = function (arr) {
+function renderText(ctx, x, y, text) {
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.fillText(text, x, y);
+}
+
+function getMaxElement(arr) {
   var maxElement = arr[0];
 
   for (var i = 1; i < arr.length; i++) {
@@ -32,28 +38,31 @@ var getMaxElement = function (arr) {
   }
 
   return maxElement;
-};
+}
 
-var randomColor = function () {
-  var r = Math.floor(Math.random());
-
-  return 'rgba(0, 0, 255,' + r + ')';
-};
+function randomColor() {
+  return 'hsla(240, 100%, 50%,' + Math.random() + ')';
+}
 
 window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+
+  renderText(ctx, CLOUD_X + 20, CLOUD_Y + 30, 'Ура вы победили!');
+  renderText(ctx, CLOUD_X + 20, CLOUD_Y + 50, 'Cписок результатов:');
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
     ctx.fillStyle = '#000';
     ctx.fillText(players[i], BAR_X + BAR_DISTANCE * i, BAR_Y);
-    if (players === 'Вы') {
+    if (players[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      ctx.fillStyle = randomColor;
+      ctx.fillStyle = randomColor();
     }
     ctx.fillRect(BAR_X + BAR_DISTANCE * i, BAR_Y - BAR_GAP_FONT, BAR_WIDTH, -(BAR_MAX_HEIGHT * times[i] / maxTime));
+
+    renderText(ctx, BAR_X + BAR_DISTANCE * i, BAR_Y - BAR_GAP_FONT - (BAR_MAX_HEIGHT * times[i] / maxTime) - 10, Math.round(times[i]));
   }
 };
